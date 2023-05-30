325080848370c87aef8e7f7b899be649fa3d3fca
Abd-al-qudus <Abd-al-qudus>
2023-05-30 22:18:49 +0000

    fixed static elements

---
+++ api/models.py
@@ -0,0 +1,14 @@
+from taskhub import db
+from flask_login import UserMixin
+from sqlalchemy import Column, String, Integer
+
+class User(UserMixin, db.Model):
+    __tablename__ = 'user'
+    
+    id = Column(Integer, primary_key=True)
+    username = Column(String(80), unique=True, nullable=False)
+    email = Column(String(100), unique=True, nullable=False)
+    password = Column(String(120), unique=True, nullable=False)
+    
+    def __repr__(self) -> str:
+        return "<User %r" %self.username
--- taskhub.py
+++ taskhub.py
@@ -1,8 +1,48 @@
 from flask import Flask
 from flask import render_template
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_bcrypt import Bcrypt
+from flask_login import (
+    #UserMixin,
+    LoginManager,
+    #login_user,
+    #logout_user,
+   # current_user,
+   # login_required,
+)
 
 
-app = Flask(__name__)
+def create_login_manager(): 
+    login_manager = LoginManager()
+    login_manager.session_protection = 'strong'
+    login_manager.login_view = 'login'
+    login_manager.login_message_category = 'info'
+    return login_manager
+
+db = SQLAlchemy()
+migrate = Migrate()
+bcrypt = Bcrypt()
+
+def create_app():
+    app = Flask(__name__)
+    app.secret_key = 'change-this-key'
+    app.config['MYSQL_HOST'] = 'localhost'
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://taskhub_user:taskhub_user_pwd@localhost/taskhub'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    # app.config['MYSQL_USER'] = 'taskhub_db_user'
+    # app.config['MYSQL_PASSWORD'] = 'taskhub_db_pwd'
+    # app.config['MYSQL_DB'] = 'taskhub_db'
+    # app.config['MYSQL_CURSORCLASS'] = 'DictCursor'
+    
+    create_login_manager().init_app(app)
+    db.init_app(app)
+    migrate.init_app(app, db)
+    bcrypt.init_app(app)
+    
+    return app
+
+app = create_app()   
 @app.route('/')
 def home():
     return render_template('3-feature.html')
